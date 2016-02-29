1a2b4e16d21eeeababc11c7a58a51330235e09aa
Michael Schneider <michael@angeleno-tech.com>
2016-02-29 10:08:39 -0800

    update search

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('../routes/index');
 var users = require('../routes/users');
-// var rates = require('../routes/rates');
+var search = require('../routes/search');
 
 var app = express();
 
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/search', search);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
