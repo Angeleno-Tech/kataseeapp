9430169710e032a35601b91cb0fa4ecce67d5d43
Michael Schneider <michael@angeleno-tech.com>
2016-02-29 08:40:33 -0800

    import some data?

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,6 @@
 var mongoose = require('mongoose');
 var rateRecord = require('./rate-record');
 
-var fs = require('fs');
-
-fs.readFile('data/krtrates.data.csv', 'utf8', function(err, contents) {
-    console.log(contents);
-});
-
-console.log('after calling readFile');
-
 var MONGO_URI = 'mongodb://localhost/katasee_db';
 
 var db = mongoose.connection;
@@ -16,16 +8,7 @@ mongoose.connect(MONGO_URI);
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-
   console.log('connected to ', MONGO_URI);
-
-
-});
-
-var krtRecord = new rateRecord();
-krtRecord.save(function (err, krtRecord) {
-	if (err) { return console.error(err) };
-	console.log("saved krtRecord");
 });
 
 module.exports = db;
